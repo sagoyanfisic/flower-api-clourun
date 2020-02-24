a2d0b9347d7d04c0b66ca894cb6b04f9ca659c2f
Yancel Salinas <sagoyanfisic1@gmail.com>
2020-02-23 20:08:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import json
 app = FastAPI()
 
 
-@app.get("/")
+#flower list
+@app.get("list/flowers/")
 async def read_root():
     with open('flower-api.json') as json_file:
         data = json.load(json_file)
     return data
 
+#flower detail
+@app.get("/detail/flower/{item_id}")
+async def read_root():
+    with open('flower-api.json') as json_file:
+        data = json.load(json_file)
+    return data
+
+#flower detail
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
